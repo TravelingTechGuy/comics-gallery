cf8bf61e1dac9bc36e70c951747bea59c6909e69
Guy Vider <guy@guyvider.com>
2020-06-19 16:51:14 -0700

    logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import photos from './photos.json';
+
+const photosFolder = '/photos';
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    let images = photos.map(img => ({
+      ...img,
+      src: `${photosFolder}/small/${img.src}`,
+      caption: img.title,
+      alt: img.title,
+      source: {
+        regular: `${photosFolder}/small/${img.src}`,
+        thumbnail: `${photosFolder}/small/${img.src}`,
+        download: `${photosFolder}/big/${img.src}`,
+        fullscreen: `${photosFolder}/medium/${img.src}`,
+      }
+    }));
+    setImages(images);
+  }, []);
+  
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Gallery photos={images} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={images}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
