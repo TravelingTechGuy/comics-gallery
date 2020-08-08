7578667b9af46146a5ff7f9f9d257d087cbe9b20
Guy Vider <guy@guyvider.com>
2020-08-07 17:02:19 -0700

    support for video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
+// import ReactPlayer from './ResponsiveYoutubePlayer';
+import ReactPlayer from 'react-player/youtube';
+
 import photos from './photos.json';
 import './App.css';
 
@@ -9,43 +12,65 @@ const photosFolder = '/photos/covers';
 function App() {
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
-  const [images, setImages] = useState(null);
+  const [images, setImages] = useState([]);
+  const [video, setVideo] = useState(null);
 
   useEffect(() => {
     const title = img => `${img.title} by ${img.artist}`;
-    let images = photos.map(img => ({
+    let imgs = photos.map(img => ({
       ...img,
       src: `${photosFolder}/${img.src}`,
       caption: title(img),
       alt: title(img),
     }));
-    setImages(images);
+    setImages(imgs);
   }, []);
 
   const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
     setViewerIsOpen(true);
-  }, []);
+    if(images[index].video) {
+      setVideo(images[index].video);
+    }
+    else {
+      setCurrentImage(index);
+    }
+  }, [images]);
 
   const closeLightbox = () => {
     setCurrentImage(0);
     setViewerIsOpen(false);
+    setVideo(null);
   };
 
   return (
     <div>
       <h1>Hand Drawn Comics Covers</h1>
-      <Gallery photos={images} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={images}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+          <Gallery photos={images} onClick={openLightbox} />
+          <ModalGateway>
+            {viewerIsOpen ?
+              <Modal onClose={closeLightbox}>
+                {video ?
+                  <ReactPlayer url={video} />
+                  :
+                  <Carousel
+                    currentIndex={currentImage}
+                    views={images}
+                    trackProps={{
+                      onViewChange: n => {
+                        if(images[n].video) {
+                          console.log('skipping', this, n);
+                          // setCurrentImage(currentImage + 1);
+                        }
+                      }
+                    }}
+                  />
+                }
+                </Modal>
+              :
+              null
+            }
+          </ModalGateway>
+      }
     </div>
   );
 }
